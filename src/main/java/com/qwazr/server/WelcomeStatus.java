e64dbb4878310148b86395f4da381a93ea4d0ba1
Emmanuel Keller <ekeller@open-search-server.com>
2018-01-05 13:59:59 +0100

    Hide empty collections

--- src/main/java/com/qwazr/server/WelcomeStatus.java
+++ src/main/java/com/qwazr/server/WelcomeStatus.java
@@ -1,5 +1,5 @@
-/**
- * Copyright 2015-2017 Emmanuel Keller / QWAZR
+/*
+ * Copyright 2015-2018 Emmanuel Keller / QWAZR
  * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,7 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- **/
+ */
 
 package com.qwazr.server;
 
@@ -33,7 +33,7 @@ import java.util.Set;
 import java.util.SortedMap;
 import java.util.TreeMap;
 
-@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class WelcomeStatus {
 
 	public final TitleVendorVersion implementation;
